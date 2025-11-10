1680f5160f102b428189a14d5cd39006d0b4891f
JullianaF <juh_jullyanna@hotmail.com>
2025-11-10 19:03:07 -0300

    TDD-REFACTOR

--- test_controlador.py
+++ test_controlador.py
@@ -10,3 +10,23 @@ def test_adicionar_item():
     assert itens[0]["titulo"] == "Ler livro"
     assert itens[0]["detalhes"] == "Ler sobre TDD em Python"
     assert itens[0]["estado"] == "pendente"
+
+def test_titulo_vazio_ou_duplicado():
+    c = ControladorDeItens()
+    c.adicionar_item("Estudar", "Aprender testes")
+
+    with pytest.raises(ValueError):
+        c.adicionar_item("", "Sem título")
+
+    with pytest.raises(ValueError):
+        c.adicionar_item("Estudar", "Duplicado")
+
+def test_finalizar_e_remover_item():
+    c = ControladorDeItens()
+    c.adicionar_item("Estudar", "Aprender TDD")
+
+    c.finalizar_item("Estudar")
+    assert c.listar_itens()[0]["estado"] == "concluído"
+
+    c.remover_item("Estudar")
+    assert len(c.listar_itens()) == 0
